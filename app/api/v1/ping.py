86659569ccd56e628f1fe139c5947ad9e0832480
vtitoff <v.titoff2010@gmail.com>
2023-01-03 22:28:22 +0300

    worked server template

---
+++ app/api/v1/ping.py
@@ -0,0 +1,14 @@
+import typing as tp
+
+from aiohttp import web
+
+from app.context import AppContext
+
+
+
+async def handle(_: web.Request, context: AppContext) -> web.Response:
+    return web.json_response(
+        {
+            'result': 'Server is ok'
+        }
+    )
---
+++ app/routes.py
@@ -0,0 +1,24 @@
+from aiohttp import web
+from app.api.v1 import ping
+from app.context import AppContext
+
+
+def wrap_handler(handler, context, request_parser=None):
+    async def wrapper(request):
+        if request_parser:
+            try:
+                request = await request_parser(request)
+            except ValueError as exc:
+                return web.json_response(
+                    {'code': 'request_error', 'error': str(exc)}, status=400
+                )
+        return await handler(request, context)
+
+    return wrapper
+
+
+def setup_routes(app: web.Application, ctx: AppContext) -> None:
+    app.router.add_get(
+        '/v1/ping',
+        wrap_handler(ping.handle, ctx),
+    )
