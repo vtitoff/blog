4cf075e5f094ce02409ec7615af2c13bb50055ca
vtitoff <v.titoff2010@gmail.com>
2023-01-14 18:03:45 +0300

    fix get services handle

--- app/api/v1/get_services_by_login.py
+++ app/api/v1/get_services_by_login.py
@@ -16,21 +16,17 @@ async def handle(req: web.Request, context: AppContext) -> web.Response:
         )
     return web.json_response(
         {
-            "result": [service.title for service in services],
+            "result": [to_response(service) for service in services],
         }
     )
-    # return web.json_response(
-    #     {
-    #         "result": to_response(services),
-    #     }
-    # )
 
 
-# def to_response(user: models.Service) -> dict:
-#     return {
-#         "login": user.login,
-#         "first_name": user.first_name,
-#         "last_name": user.last_name,
-#         "user_info": user.user_info,
-#         "contacts": user.contacts,
-#     }
+def to_response(service: models.Service) -> dict:
+    return {
+        "id": str(service.id),
+        "title": service.title,
+        "description": service.description,
+        "cost": service.cost,
+        "currency": service.currency,
+        "user_login": service.user_login,
+    }
