d5b8c242e995a5625f39661b11a9a91e71c4aee5
vtitoff <v.titoff2010@gmail.com>
2023-01-14 16:46:47 +0300

    change insert sql

--- scripts/insert_data.sql
+++ scripts/insert_data.sql
@@ -1,14 +1,14 @@
 INSERT INTO users (login, first_name, last_name, user_info, contacts) VALUES
-('sweetly', 'Ratliff', 'Sweet', 'дизайнер', ''),
-('alstrong', 'Aline', 'Strong', 'дизайнер', ''),
-('seren', 'Serena', 'Weber', 'редактор', ''),
-('brady', 'Brady', 'Cline', 'менеджер', ''),
-('grant', 'Marisol', 'Grant', 'учитель', '');
+('sweetly', 'Ratliff', 'Sweet', 'дизайнер', null),
+('alstrong', 'Aline', 'Strong', 'дизайнер', null),
+('seren', 'Serena', 'Weber', 'редактор', null),
+('brady', 'Brady', 'Cline', 'менеджер', null),
+('grant', 'Marisol', 'Grant', 'учитель', null);
 
 
 INSERT INTO user_services (id, title, description, cost, currency, user_login) VALUES
-(gen_random_uuid(), 'логотип', '', 10, '$', 'sweetly'),
-(gen_random_uuid(), 'логотип', '', 15, '$', 'alstrong'),
-(gen_random_uuid(), 'написать статью', '', 20, '$', 'seren'),
-(gen_random_uuid(), 'ведение проекта', '', 50, '$', 'brady'),
-(gen_random_uuid(), 'занятие', '', 10, '$', 'grant')
+(gen_random_uuid(), 'логотип', null, 10, '$', 'sweetly'),
+(gen_random_uuid(), 'логотип', null, 15, '$', 'alstrong'),
+(gen_random_uuid(), 'написать статью', null, 20, '$', 'seren'),
+(gen_random_uuid(), 'ведение проекта', null, 50, '$', 'brady'),
+(gen_random_uuid(), 'занятие', null, 10, '$', 'grant')
